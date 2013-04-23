d16288118b54dac493f2261267cbecc19cf28a99
Chris Down <chris@chrisdown.name>
2013-04-23 16:43:58 +0800

    Clean up email JS

--- js/email.js
+++ js/email.js
@@ -3,8 +3,12 @@ layout: none
 ---
 
 document.addEventListener("DOMContentLoaded", function() {
-    var el = document.getElementById("email");
-    var address = [ "{{ site.personal.email.account }}", "{{ site.url | split: '/' | last }}" ].join("@");
+    "use strict";
+    var el, address;
+
+    el = document.getElementById("email");
+    address = [ "{{ site.personal.email.account }}", "{{ site.url | split: '/' | last }}" ].join("@");
+
     el.href = "mailto:" + address
     el.innerHTML = address
 });
