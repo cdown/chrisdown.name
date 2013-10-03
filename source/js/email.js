a2529b47d7cfc353067b70f7bbea09f61293e191
Chris Down <chris@chrisdown.name>
2013-10-03 13:04:18 +0800

    All hail the return of transparent e-mail obfuscation!
    For the timebeing I am having to use a mail server with inadequate spam
    filtering, so...

---
+++ source/js/email.js
@@ -0,0 +1,13 @@
+---
+---
+
+document.addEventListener("DOMContentLoaded", function() {
+    "use strict";
+    var el, address;
+
+    el = document.getElementById("email");
+    address = [ "{{ site.personal.email.account }}", "{{ site.url | split: '/' | last }}" ].join("@");
+
+    el.href = "mailto:" + address
+    el.innerHTML = address
+});
