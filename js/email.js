e723061ecd6914f52430d4562025cfaefd0dff45
Chris Down <chris@chrisdown.name>
2018-04-20 13:00:40 +0100

    Inline JS per-page
    It's quite clear that chaining these together, especially for
    analytics.js, is a non-trivial cause of increased latency to DOM load
    finished.

--- js/email.js
+++
@@ -1,6 +0,0 @@
-InstantClick.on('change', function() {
-  var el = document.getElementById('footer-email')
-  var address = ['chris', 'chrisdown.name'].join('@');
-  el.href = 'mailto:' + address;
-  el.innerHTML = address;
-});
