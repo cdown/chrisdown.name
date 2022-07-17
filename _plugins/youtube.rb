ca188d18cd1eda5ee6bfd03fadb2e93435ae713a
Chris Down <chris@chrisdown.name>
2022-07-17 06:24:27 +0100

    Revert "youtube: Move to lite-yt-embed"
    Too many problems, eg. the play button randomly deciding to disappear.
    
    This reverts commit d7809af1e6120c84058be7e25035a5cb88af5176.

--- _plugins/youtube.rb
+++ _plugins/youtube.rb
@@ -12,21 +12,10 @@ class YouTube < Liquid::Tag
   end
 
   def render(context)
-    %Q{
-<script type="text/javascript">
-var ytcss_id = "ytcss";
-if (!document.getElementById(ytcss_id)) {
-  var link = document.createElement("link");
-  link.href = "/css/lite-yt-embed.css";
-  link.id = ytcss_id;
-  link.type = "text/css";
-  link.rel = "stylesheet";
-  document.getElementsByTagName("head")[0].appendChild(link);
-}
-</script>
-<script src="/js/lite-yt-embed.js"></script>
-<lite-youtube videoid="#{@id}" playlabel="Play"></lite-youtube>
-    }
+    "<div class=\"youtube-container\">
+         <iframe allowfullscreen=\"allowfullscreen\"
+          src=\"//www.youtube.com/embed/#{@id}\"> </iframe>
+     </div>"
   end
 
   Liquid::Template.register_tag "youtube", self
