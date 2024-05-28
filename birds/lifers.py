2bce9b743f37d533bb4cebc100d20307bddaec08
Chris Down <chris@chrisdown.name>
2024-05-28 11:22:12 +0100

    birds: Store cache of countries seen

--- birds/lifers.py
+++ birds/lifers.py
@@ -4,8 +4,10 @@ import csv
 import json
 import time
 import sys
-from datetime import datetime, timezone
+from datetime import datetime
 from geopy.geocoders import Nominatim
+
+CACHE_FILE = "loc_cache.json"
 
 geolocator = Nominatim(user_agent="lifers")
 
@@ -14,39 +16,57 @@ def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
 
-def get_country(latitude, longitude, cache):
-    tolerance_deg = 0.1  # 11.1km
+def load_cache():
+    try:
+        with open(CACHE_FILE, "r", encoding="utf-8") as f:
+            return json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError) as e:
+        eprint(f"Problem loading file: {e}")
+        return {"coordinates": {}, "countries": []}
 
-    lat_rounded = round(float(latitude), 1)
-    long_rounded = round(float(longitude), 1)
 
-    eprint("Checking ({}, {})".format(lat_rounded, long_rounded))
+def save_cache(cache):
+    # Make determinstic so source control deltas are small
+    cache["coordinates"] = dict(sorted(cache["coordinates"].items()))
+    with open(CACHE_FILE, "w", encoding="utf-8") as f:
+        json.dump(cache, f, indent=4)
 
-    for (cached_lat, cached_long), country in cache.items():
-        if (
-            abs(lat_rounded - cached_lat) <= tolerance_deg
-            and abs(long_rounded - cached_long) <= tolerance_deg
-        ):
-            eprint(f"Found in cache: {country}")
-            return country
+
+def round_coordinate(coord):
+    whole, fraction = coord.split(".")
+    return f"{whole}.{fraction[:1]}"
+
+
+def get_country(lat, lng, cache):
+    lat_rounded = round_coordinate(lat)
+    lng_rounded = round_coordinate(lng)
+    coord_key = f"{lat_rounded},{lng_rounded}"
+
+    eprint(f"Checking ({lat_rounded}, {lng_rounded})")
+
+    if coord_key in cache["coordinates"]:
+        country_index = cache["coordinates"][coord_key]
+        country = cache["countries"][country_index]
+        eprint(f"Found in cache: {country}")
+        return country
 
     eprint("Not in cache, doing lookup")
 
     time.sleep(1)  # No heavy uses (an absolute maximum of 1 request per second).
-    location = geolocator.reverse(
-        (latitude, longitude), exactly_one=True, language="en"
-    )
-    address = location.raw["address"]
-    country = address.get("country", "Unknown")
+    location = geolocator.reverse((lat, lng), exactly_one=True, language="en")
+    country = location.raw["address"]["country"]
 
-    cache[(lat_rounded, long_rounded)] = country
+    if country not in cache["countries"]:
+        cache["countries"].append(country)
+    country_index = cache["countries"].index(country)
+    cache["coordinates"][coord_key] = country_index
     eprint(f"Got {country}")
     return country
 
 
 def process_csv(file_path):
     first_sightings = {}
-    cache = {}
+    cache = load_cache()
 
     with open(file_path, mode="r", encoding="utf-8") as file:
         reader = csv.DictReader(file)
@@ -63,16 +83,21 @@ def process_csv(file_path):
                     country_name,
                 ]
 
+    save_cache(cache)
     sightings_list = list(reversed(first_sightings.values()))
 
     return json.dumps(sightings_list)
 
 
-if __name__ == "__main__":
+def main():
     if len(sys.argv) != 2:
         eprint("Missing CSV")
         sys.exit(1)
 
     file_path = sys.argv[1]
     js_array = process_csv(file_path)
-    print(js_array)
+    print(f"const sightings = {js_array};")
+
+
+if __name__ == "__main__":
+    main()
